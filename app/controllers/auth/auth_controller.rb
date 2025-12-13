b11e9e767f633035abc70a3ee087ca39d7231361
Quintin Russell <quinn.j.russell@gmail.com>
2025-12-13 09:40:17 -0800

    added simple auth

--- app/controllers/auth/auth_controller.rb
+++ app/controllers/auth/auth_controller.rb
@@ -1,30 +1,44 @@
-class ApplicationController < ActionController::API
-  def create_auth_token
-    user = User.by_email(auth_params[:email])
-    if user&.authenticate(auth_params[:password])
-      token = JsonWebToken.encode(user_id: user.id)
+module Auth
+  class AuthController < ApplicationController
+    skip_before_action :authenticate_request, only: [:create_auth_token]
+    def create_auth_token
+      user = User.by_email(email_param)
+      if user&.authenticate(password_param)
+        token = encode_token({ user_id: user.id })
+        set_jwt_cookie(token)
 
-      cookies.signed[:jwt] = {
+        @current_user = user
+      else
+        render json: { error: 'Invalid email or password' }, status: :unauthorized
+      end
+    end
+
+    def destroy_auth
+      cookies.delete(:auth)
+      render json: { message: 'Logged out' }, status: :ok
+    end
+
+    private
+
+    def email_param
+      params.require(:email)
+    end
+
+    def password_param
+      params.require(:password)
+    end
+
+    def encode_token(payload)
+      JWT.encode(payload, Rails.application.credentials.jwt_secret, "HS256")
+    end
+
+    def set_jwt_cookie(token)
+      cookies.signed[:auth] = {
         value: token,
         httponly: true,
         secure: Rails.env.production?,
-        same_site: :strict
+        same_site: :none
       }
-
-      render json: { message: 'Logged in successfully' }, status: :ok
-    else
-      render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
-  end
-
-  def destroy_auth
-    cookies.delete(:jwt)
-    render json: { message: 'Logged out' }, status: :ok
-  end
-
-  private
-
-  def auth_params
-    params.require(:email, :password)
   end
 end
