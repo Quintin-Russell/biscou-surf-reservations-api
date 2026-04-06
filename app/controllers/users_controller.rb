a46a4f976e9efde18a0e957b16d73642378f6ad8
Quintin Russell <quinn.j.russell@gmail.com>
2026-04-06 00:53:14 -0700

    cleaner exceptions in application controller, user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     permitted_params = params.permit(:first_name, :last_name, :email, :password, :nationality, :phone_number)
     required_params = ["first_name", "last_name", "email", "password", "nationality", "phone_number"]
     missing_params = required_params.reject{ |req_param| permitted_params[req_param].present? }
-    throw Exceptions::MissingParameters if missing_params.any?
+    raise Exceptions::MissingParameters if missing_params.any?
     permitted_params
   end
 end
